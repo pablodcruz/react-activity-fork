577393953866d502459b0c6e08bf717ee0da34c7
ThachK <kimmy.thach.asian@gmail.com>
2023-02-28 16:45:26 -0600

    added delete button

--- src/components/PokemonBox/PokemonBox.tsx
+++ src/components/PokemonBox/PokemonBox.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import { Pokemon } from '../../models/Pokemon'
+import { Pokemon } from "../../models/Pokemon";
 
-export default function PokemonBox(props:Pokemon) {
-  return (
-         <div>
-            <img src={props.img}/>
-            <h4>{props.name}</h4>
-            <p>Level: {props.level}</p>
-            <p>Health: {props.health}</p>
-            <p>Damage: {props.damage}</p>
-         </div>
-         )
+interface Props extends Pokemon {
+	onDelete?: () => void;
+}
+
+export default function PokemonBox(props: Props) {
+	const { name, img, level, health, damage, onDelete } = props;
+
+	return (
+		<div className="pokemon-box">
+			<img src={img} alt={name} />
+			<div className="pokemon-details">
+				<h4>{name}</h4>
+				<p>Level: {level}</p>
+				<p>Health: {health}</p>
+				<p>Damage: {damage}</p>
+				{onDelete && <button onClick={onDelete}>Delete</button>}
+			</div>
+		</div>
+	);
 }
